330c47926fbbd374cf25f1b922e85445ddb3548a
kingJTao <951086498@qq.com>
2018-09-22 12:00:29 +0800

    插入结点未完成

--- BiMinusTree.c
+++ BiMinusTree.c
@@ -4,23 +4,39 @@
 int search(BSTree t,int k){
 	int pos = 0;
 	
-	for(int i = 1; i <= t->keynum; ++i){
-		if( t->key[i] == k ){
-			pos = i;
-			break;
-		}else if( t->key[i] < k ){
-			if( i+1>t->keynum || t->key[i+1]>k ){
+	if( t ){
+		for(int i = 1; i <= t->keynum; ++i){
+			if( t->key[i] == k ){
 				pos = i;
 				break;
+			}else if( t->key[i] < k ){
+				if( i+1>t->keynum || t->key[i+1]>k ){
+					pos = i;
+					break;
+				}
 			}
 		}
 	}
 
 	return pos;
 }
-/*************************end*****************************/
 
-Result SearchBTree(BTree T,int k){
+void insert(BSTree t,int pos,int k){
+	for(int inst = t->fieldNum; inst > pos; --inst){
+		t->key[inst] = t->key[inst-1];
+		t->ptr[inst] = t->ptr[inst-1];
+	}
+	//插入结点
+	t->key[pos] = k;
+	t->ptr[pos] = t->ptr[pos-1];
+	++t->keynum;
+}
+/*************************end*****************************/
+void setBTreeLayer(int num){
+	layer = num;
+}
+
+Result SearchBTree(BTree T,int k,int *mode){
 	Result rt = {NULL,0,0};
 	BTree p = T,
 		  q = NULL;
@@ -32,17 +48,50 @@ Result SearchBTree(BTree T,int k){
 		if( i>0 && p->key[i]==k ){
 			rt.pt = p;
 			rt.i = i;
-			rt.tag = 1;
+			rt.tag = 1;i
 			found = 1;
+			break;
 		}else{
-			p = p->ptr[i];
+			if( !mode ){
+				p = p->ptr[i];
+			}else if( *mode == 1){
+				break;
+			}
 		}
 	}
 
 	return rt;
 }
 
-void InsertBTree(BTree *T,int k){
+void InsertBTree(BTree *T,int k,BTree q,int pos){
+	if( q ){//插入结点非空
+	}else{
+		if( !(*T) ){//插入为空
+			*T = (BTree)malloc(sizeof(BTNode));
+			if( *T ){
+				(*T)->key = (int*)malloc((layer+1) * sizeof(int));
+				(*T)->ptr = (BTree)malloc(layer+1);
+				if((*T)->key && (*T)->ptr){
+					(*T)->parent = NULL;
+					(*T)->keynum = 1;
+					(*T)->key[1] = k;
+					memset((*T)->ptr,NULL,layer+1);
+				}
+			}
+		}else{
+			int i = search(*T,k);
+
+			if( i>0 && (*T)->key[i]==k ){
+				return;
+			}else{
+				if( !((*T)->ptr[i]) ){//插入本结点
+
+				}else{
+					InsertBTree();
+				}
+			}
+		}
+	}
 }
 
 void DeleteBTree(BTree *T,int k){
--- BiMinusTree.h
+++ BiMinusTree.h
@@ -1,10 +1,12 @@
-#define m 3
+#ifndef _MY_BTREE_NUM
+int layer = 0;
+#endif
 
 typedef struct BTNode{
 	int keynum;						// 结点中关键字个数，即结点的大小
 	struct BTNode *parent;			// 指向双亲结点
-	int key[m+1];					// 关键字向量，0号单元未用
-	struct BTNode *ptr[m+1];		// 子树指针向量
+	int *key;						// 关键字向量
+	struct BTNode **ptr;			// 子树指针向量
 }BTNode,*BTree;
 
 typedef struct{
@@ -13,11 +15,14 @@ typedef struct{
 	int tag;						// 0:查找失败,1:查找成功
 }Result;
 
+//设置树的阶数
+void setBTreeLayer(int num);
+
 //查找关键字
-Result SearchBTree(BTree T,int k);
+Result SearchBTree(BTree T,int k,int *mode);
 
 //插入结点
-void InsertBTree(BTree *T,int k);
+void InsertBTree(BTree *T,int k,BTree q,int pos);
 
 //删除结点
 void DeleteBTree(BTree *T,int k);
